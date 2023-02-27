e82b7052feff79d72cbfe02ad7a9b348330df3a1
Camila Medeiros <89259285+camilamedeir0s@users.noreply.github.com>
2023-02-27 09:25:24 -0300

    first commit

---
+++ aws_challenge/sam-app/src/handlers/get-by-id.mjs
@@ -0,0 +1,33 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';
+const client = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+const tableName = process.env.SAMPLE_TABLE;
+
+export const getByIdHandler = async (event) => {
+  if (event.httpMethod !== 'GET') {
+    throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
+  }
+ 
+  const id = event.pathParameters.id;
+ 
+  var params = {
+    TableName : tableName,
+    Key: { id: id },
+  };
+
+  try {
+    const data = await ddbDocClient.send(new GetCommand(params));
+    var item = data.Item;
+  } catch (err) {
+    console.log("Error", err);
+  }
+ 
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(item)
+  };
+
+  return response;
+}
